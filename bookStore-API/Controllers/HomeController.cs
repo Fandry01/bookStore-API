bcf926a7598ad509dfc1e900fd3a346ed4a254b8
F baffour <f.baffour@outlook.com>
2020-10-21 20:36:45 +0200

    Add project files.

---
+++ bookStore-API/Controllers/HomeController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using bookStore_API.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace bookStore_API.Controllers
+{
+    /// <summary>
+    /// this is a test api controller
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+
+        private readonly IloggerService _logger;
+
+        public HomeController(IloggerService logger)
+        {
+            _logger = logger;
+
+        }
+        /// <summary>
+        ///  get values 
+        /// </summary>
+        /// <returns></returns>
+        // GET: api/<HomeController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            _logger.LogInfo("accessed Home Controller");
+            return new string[] { "value1", "value2" };
+        }
+        /// <summary>
+        ///  get a value
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        // GET api/<HomeController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            _logger.LogDebug("Got a Value");
+            return "value";
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        // POST api/<HomeController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+            _logger.LogError("this is an error");
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="value"></param>
+
+        // PUT api/<HomeController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        // DELETE api/<HomeController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _logger.LogWarn("this is a warningr");
+        }
+    }
+}
